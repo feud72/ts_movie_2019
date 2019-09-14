87a000913a1550bc500208bacbbd45f92c8bf46c
feud72 <feud72@gmail.com>
2019-09-15 00:14:48 +0900

    #3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 
-interface IFood {
-  fav: string;
-  image: string;
+interface IState {
+  count: number
 }
 
-const Food: React.FunctionComponent<IFood> = ({fav, image}) => {
-  return (
-    <>
-      <h1>I like {fav}</h1>
-      <img src={image} alt={fav} />
-    </>
-  );
-};
+class App extends React.Component<{}, IState> {
+  state = {
+    count : 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  }; 
+  render () {
+    return (<div><h1>The number is: {this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>Minus</button></div>
+    );
+  }
 
-const foodILike = [
-  {
-    name: 'Kimchi',
-    image:
-      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-  },
-  {
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-  },
-  {
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-  },
-  {
-    name: 'Doncasu',
-    image:
-      'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-  },
-  {
-    name: 'Kimbap',
-    image:
-      'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-  },
-];
-
-interface IDish {
-  name: string;
-  image: string;
 }
-
-const renderFood : React.FunctionComponent<IDish> = ({name, image})=> {
-return <Food fav={name} image={image} />
-}
-
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      {foodILike.map(stuff => renderFood(stuff))}
-    </div>
-  );
-};
 
 export default App;

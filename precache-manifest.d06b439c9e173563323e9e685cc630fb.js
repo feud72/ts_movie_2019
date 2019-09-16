9ee0dd3459e6626bc6d762141bd755c8d7768ca5
feud72 <feud72@gmail.com>
2019-09-16 14:17:26 +0900

    Updates

---
+++ precache-manifest.d06b439c9e173563323e9e685cc630fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11376bdff3354720518290fcd05ec906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb20447be3294ab9f33",
+    "url": "/static/css/main.0ef1261f.chunk.css"
+  },
+  {
+    "revision": "5ed7002590d1267d9915",
+    "url": "/static/js/2.a8f8d4d4.chunk.js"
+  },
+  {
+    "revision": "ecb20447be3294ab9f33",
+    "url": "/static/js/main.abe4138a.chunk.js"
+  },
+  {
+    "revision": "60ab293b60b44f5bb796",
+    "url": "/static/js/runtime~main.ad958ee8.js"
+  }
+]);

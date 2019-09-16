cd7844e30e1930d4322350729624be5abd8ca2b8
feud72 <feud72@gmail.com>
2019-09-16 13:04:51 +0900

    #4.2

--- src/App.tsx
+++ src/App.tsx
@@ -37,21 +37,27 @@ class App extends React.Component {
   render() {
     const {isLoading, movies} = this.state;
     return (
-      <div>
-        {isLoading
-          ? 'Loading...'
-          : movies.length > 0 &&
-            movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.length > 0 &&
+              movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.tsx
+++ src/Movie.tsx
@@ -15,7 +15,16 @@ const Movie: React.FunctionComponent<IProps> = ({
   summary,
   poster,
 }) => {
-  return <h4>{title}</h4>;
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
